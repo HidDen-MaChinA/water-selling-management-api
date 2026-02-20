91df23069a3e6ff2ff37c4d54c06f389eb2b641f
HidDen-MaChinA <hei.franco.3@gmail.com>
2026-02-20 09:50:05 +0100

    feat: add breejs for cron jobs worker

---
+++ rollup.workers.config.ts
@@ -0,0 +1,22 @@
+import typescript from '@rollup/plugin-typescript';  // Compiles TypeScript files
+import terser from '@rollup/plugin-terser';          // Handles minification to make the bundle smaller
+import { globSync } from "glob";
+
+const workers = globSync('./src/jobs/workers/**/*.ts');
+workers.push("./src/jobs/index.ts");
+
+export default {
+  input: workers,
+  output: {
+    dir: "dist/jobs",
+    format: "esm", // Output format: ES module
+    sourcemap: true,
+    preserveModules: true
+  },
+  plugins: [
+    terser(), // Use terser for minification to make the bundle smaller
+    typescript({
+      tsconfig: "./tsconfig.workers.json", // Use this tsconfig file to configure TypeScript compilation
+    }),
+  ],
+};
---
+++ src/jobs/index.ts
@@ -0,0 +1,17 @@
+import Bree from "bree";
+import Cabin from "cabin"
+import path from "path";
+
+
+
+const bree = new Bree({
+    logger: new Cabin(),
+    root:path.resolve("dist/jobs/workers"),
+    jobs: [
+        {
+            name: "generateAnalyticsDataWorker",
+        }
+    ]
+})
+
+bree.start();
